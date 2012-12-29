117d598005c81097b775f075745fe3cdc9b8a3a9
Syed Arsalan Hussain Shah <arsalan@buddyexpress.net>
2012-12-29 10:16:30 -0800

    engine settings file

---
+++ bframework/engine/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Buddyexpress Framework Core
+ *
+ * @package   Bframework
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2012 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://bserver.buddyexpress.net
+ * @Contributors http://www.buddyexpress.net/bframework/contributors.b
+ */
+$Bframework->baseurl = 'http://bframework.buddyexpress.net/';
+$Bframework->sitebron = 1355848839;
+ini_set('display_errors','off');
