e3c6150ef28df2679580f29c3c1dd3d2fa41f007
Syed Arsalan Hussain Shah <arsalan@buddyexpress.net>
2012-12-29 10:08:01 -0800

    application settings file

---
+++ application/application.php
@@ -0,0 +1,28 @@
+<?php 
+/**
+ * Buddyexpress Webdevelopment Core
+ *
+ * @package   Bframework
+ * @subpackage bwebdev
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2012 BUDDYEXPRESS.
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @link      http://www.buddyexpress.net
+ */
+/*
+* The application version
+*/ 
+$BframeworkVersion = '1.0.0';
+/*
+* The application release
+*/ 
+$BframeworkRelease = 'December 2012';
+/*
+* The Application Name
+*/ 
+$BframeworkAppName = 'Dummy Application for Bframework';
+/*
+* The Application admin url
+* example  admin@mysite.com
+*/ 
+$BframeworkadminEmail = '';
