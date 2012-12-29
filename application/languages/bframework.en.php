0e3143034442b5ed12ba6ebb57f1a1fd0adfaf7c
Syed Arsalan Hussain Shah <arsalan@buddyexpress.net>
2012-12-29 10:08:22 -0800

    demo language file for app

---
+++ application/languages/bframework.en.php
@@ -0,0 +1,9 @@
+<?php
+
+$englsih = array(
+'default:msg' => "Hi there, this is default index file you can edit it in application/template/index.php, The full path to this file is ".bframework_get_approot_path().'template/index.php', 
+'welcome:home' => 'Welcome to the website'
+);
+
+bframework_register_languages($englsih);
+?>
