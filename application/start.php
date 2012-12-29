34e4ebed1fd57e6e2b472d61c0278051253ae992
Syed Arsalan Hussain Shah <arsalan@buddyexpress.net>
2012-12-29 10:07:45 -0800

    application start file

---
+++ application/start.php
@@ -0,0 +1,27 @@
+<?php 
+/**
+ * Bframework Application start file
+ *
+ * @package   Bframework
+ * @subpackage my application 
+ * @author    Buddyexpress Core Team <admin@buddyexpress.net
+ * @copyright 2012 BUDDYEXPRESS.
+ * @ApplicationCopyright < Your Copyrights’ >
+ * @license   Buddyexpress Public License http://www.buddyexpress.net/Licences/bpl/ 
+ * @ApplicationLicense < Your License >
+ * @link      http://www.buddyexpress.net
+ */
+
+require_once(dirname(dirname(__FILE__)).'/bframework/buddyexpress.php'); 
+
+/* My Custom Functions Goes Here */
+
+function mycustomfunction(){
+}
+/* My Custom Functions END */
+
+bframework_resgiser_css('css.php');
+/* This is default index page, you can change the contents in template/index.php */
+bframework_page_handler(array('handler' => 'index', 'file' => bframework_get_approot_path().'pages/index.php'));
+
+?>
