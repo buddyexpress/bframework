0c305e940ffc04d2c6930e72480ca6eb3b55e4c9
Arsalan <admin@buddyexpress.net>
2013-07-01 10:14:29 -0700

    fixed style

--- bframework/libraries/bframework.exception.php
+++ bframework/libraries/bframework.exception.php
@@ -12,12 +12,11 @@
  
 /*
 * Bframework exception setup
-* bframework_register_exception(<message>) 
-* Uses its works only by throw a new exception 
+* bframework_register_exception(<message>), <message>
 */  
 function bframework_register_exception($exception){
 if(isset($exception)){
- echo bframework_div(array('attr' => array('class' => 'bframework_error_red','align' => 'center'))); 
+echo bframework_div(array('attr' => array('style' => 'background:#F6CBCA;border:1px #CB2026 solid;color:#CB2026;font-weight:bold;','class' => 'bframework_error_red','align' => 'center'))); 
  echo "Exception: " , $exception->getMessage().' '.$exception->getFile()." Line number ".$exception->getLine();
  echo bframework_div(array('close' => 'close'));
  exit();
