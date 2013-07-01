45b02df84ae4405f25c725b614df950d986cb50a
Arsalan <admin@buddyexpress.net>
2013-07-01 10:04:11 -0700

    page layout file

--- bframework/views/layout/default.php
+++ bframework/views/layout/default.php
@@ -18,22 +18,23 @@ $title = bframework_get_app('name');
 } else {
 $title = bframework_get_app('name') . " : " . $page['title'];
 }
+$meta_attr = array(
+'BframeworkVersion' => bframework_get_version(),
+'BframeworkRelease' => bframework_get_release(),
+'ApplicationVersion' => bframework_get_app_version(),
+'ApplicationRelease' => bframework_get_app_release(),
+);
 header("Content-type: text/html; charset=UTF-8");
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<?php echo bframework_meta(array('attr' => array('http-equiv' => "Content-Type", 'content' => "text/html; charset=utf-8"))); ?>
-
-<?php echo bframework_meta(array('attr' => array('name' => "BframeworkVersion", 'content' => bframework_get_version()))); ?>
-
-<?php echo bframework_meta(array('attr' => array('name' => "BframeworkRelease", 'content' => bframework_get_release()))); ?>
-
-<?php echo bframework_meta(array('attr' => array('name' => "ApplicationVersion", 'content' => bframework_get_app_version()))); ?>
-
-<?php echo bframework_meta(array('attr' => array('name' => "ApplicationRelease", 'content' => bframework_get_app_release()))); ?>
-
+<?php echo bframework_meta(array('attr' => array('http-equiv' => "Content-Type", 'content' => "text/html; charset=utf-8"))); 
+foreach($meta_attr as $s => $v){
+echo bframework_meta(array('attr' => array('name' => $s, 'content' => $v)));
+}
+?>
 <title><?php echo $title; ?></title>
 <?php echo bframework_inc_css($css);?>
 <?php echo $page['head']; ?>
