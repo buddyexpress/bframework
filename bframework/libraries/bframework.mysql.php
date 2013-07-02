90db0c69581873c2054e2f00d983b1acb785ec00
Arsalan <arsalan@buddyexpress.net>
2013-07-01 20:48:15 -0700

    mysql lib

--- bframework/libraries/bframework.mysql.php
+++ bframework/libraries/bframework.mysql.php
@@ -10,3 +10,20 @@
  * @Contributors http://www.buddyexpress.net/bframework/contributors.b
  */
  
+define('db_host', 'localhost');
+define('db_hostip', '127.0.0.1');
+
+function bframework_connect_db($params = array()){
+if(empty($params['host'])){ 
+      $params['host'] = db_host;
+}  if(empty($params['user'])){ 
+      $params['user'] = 'root';
+} if(empty($params['password'])){ 
+      $params['password'] = '';
+} if(empty($params['db'])){ 
+   throw new DatabaseException('Can not connect to database');
+} mysql_connect ($params['host'],$params['password'],$params['user']) or die('Cannot connect to MySQL: ' . mysql_error());
+mysql_select_db ($params['db']) or die ('Cannot connect to the database: ' . mysql_error());
+}
+
+
